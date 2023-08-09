52d611a969de8b15488ac8c225327b0ba3d07bcf
Yachitra Sivakumar <92534699+YayaSivakumar@users.noreply.github.com>
2023-08-09 18:45:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,26 @@
-'''
-import ast
-import os.path
-from xml.dom import minidom
-
-
-#### ONLY ONE OBJECT PER IMAGE IS ASSUMED ####
-
-out_dir = './out'
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-
-file = minidom.parse('annotations.xml')
-
-images = file.getElementsByTagName('image')
-
-for image in images:
-    width = int(image.getAttribute('width'))
-    height = int(image.getAttribute('height'))
-    name = image.getAttribute('name')
-    elem = image.getElementsByTagName('points')
-    bbox = image.getElementsByTagName('box')
-    for b in bbox:
-        xtl = int(float(b.attributes['xtl'].value))
-        ytl = int(float(b.attributes['ytl'].value))
-        xbr = int(float(b.attributes['xbr'].value))
-        ybr = int(float(b.attributes['ybr'].value))
-    
-    w = xbr - xtl
-    h = ybr - ytl
-
-    label_file = open(os.path.join(out_dir, name[:-4] + '.txt'), 'w')
-
-    for e in elem:
-
-        label_file.write('0 {} {} {} {} '.format(str((xtl + (w / 2)) / width), str((ytl + (h / 2)) / height),
-                                                 str(w / width), str(h / height)))
-
-        points = e.attributes['points']
-        points = points.value.split(';')
-        points_ = []
-        for p in points:
-            p = p.split(',')
-            p1, p2 = p
-            points_.append([int(float(p1)), int(float(p2))])
-        for p_, p in enumerate(points_):
-            label_file.write('{} {} 2'.format(p[0] / width, p[1] / height))
-            if p_ < len(points_) - 1:
-                label_file.write(' ')
-            else:
-                label_file.write('\n')
-'''
-
 from ultralytics import YOLO
+import numpy
 import cv2
+import math
 
-model_path = 'best.pt'
+model_path = 'pose1.pt'
 model = YOLO(model_path)
+def calc_angle(list_of_points):
+    angles = []
+    for index in range(0, 8, 4):
+        # under leg
+        x1, y1 = list_of_points[index]
+        x2, y2 = list_of_points[index+1]
+        m1 = (x2 - x1) / (y2 - y1)
+        # upper leg
+        x1, x2 = list_of_points[index+2]
+        y1, y2 = list_of_points[index+3]
+        m2 = (x2 - x1) / (y2 - y1)
+        m = abs((m2-m1)/(1+(m2*m1)))
+        q = numpy.arctan(m)
+        q = math.degrees(q)
+        angles.append(q)
+    return angles
 def calculate_points(image_path):
     image_path = image_path
     img = cv2.imread(image_path)
@@ -73,6 +38,17 @@ def calculate_points(image_path):
 
     cv2.imshow('img', img)
     cv2.waitKey(0)
+
     return list_of_points
 
-print(calculate_points('f.jpg'))
+if __name__ == "__main__":
+    list_of_points = (calculate_points('feet.jpg'))
+    left_foot, right_foot = calc_angle(list_of_points)
+    print('left foot angle:', round(left_foot, 2))
+    print('right foot angle: ', round(right_foot, 2))
+
+
+
+
+
+
